e0286b7e1c5c50122c74f9458037b51c957e18d4
AndrewGalynin <galynin69@gmail.com>
2021-10-29 10:29:52 +0300

    Changes

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -21,15 +21,7 @@ int main()
 		x += h;
 	}
 
-	cout << "do...while" << endl;
-	x = min;
-	do
-	{
-		y = pow(x, 3) + pow(x, 1 / 2) - 3 * c;
-		cout << setw(10) << "x= " << x << setw(10) << "y= " << y << endl;
-		x += h;
-	}
-	while (x <= max + h / 2);
+	
 }
 
 
